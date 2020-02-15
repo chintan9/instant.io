e922aae2fb5ae44a23d39d94d762971418516397
Restyled.io <commits@restyled.io>
2020-02-15 00:00:29 +0000

    Restyled by prettier

--- client/util.js
+++ client/util.js
@@ -1,49 +1,48 @@
-var escapeHtml = require('escape-html')
-var logElem = exports.logElem = document.querySelector('.log')
-var logHeading = document.querySelector('#logHeading')
-var speed = document.querySelector('.speed')
+var escapeHtml = require("escape-html");
+var logElem = (exports.logElem = document.querySelector(".log"));
+var logHeading = document.querySelector("#logHeading");
+var speed = document.querySelector(".speed");
 
-exports.log =
-    function log(item, unsafe) {
-  logHeading.style.display = 'block'
+exports.log = function log(item, unsafe) {
+  logHeading.style.display = "block";
 
-  var p = document.createElement('p')
-  if (unsafe) p.innerHTML = item
-  else p.textContent = item
-  logElem.appendChild(p)
-  return p
-}
+  var p = document.createElement("p");
+  if (unsafe) p.innerHTML = item;
+  else p.textContent = item;
+  logElem.appendChild(p);
+  return p;
+};
 
-    exports.unsafeLog = function unsafeLog(
-                            item) { return exports.log(item, true) }
+exports.unsafeLog = function unsafeLog(item) {
+  return exports.log(item, true);
+};
 
-                        exports.appendElemToLog =
-        function append(item) {
-  logHeading.style.display = 'block'
+exports.appendElemToLog = function append(item) {
+  logHeading.style.display = "block";
 
-  logElem.appendChild(item)
-  exports.lineBreak()
-  return item
-}
+  logElem.appendChild(item);
+  exports.lineBreak();
+  return item;
+};
 
-        exports.lineBreak =
-            function
-            lineBreak() { logElem.appendChild(document.createElement('br')) }
+exports.lineBreak = function lineBreak() {
+  logElem.appendChild(document.createElement("br"));
+};
 
-            // replace the last P in the log
-            exports.updateSpeed = function updateSpeed(
-                                      str) { speed.innerHTML = str }
+// replace the last P in the log
+exports.updateSpeed = function updateSpeed(str) {
+  speed.innerHTML = str;
+};
 
-                                  exports.warning =
-                function warning(err) {
-  console.error(err.stack || err.message || err)
-  return exports.log(escapeHtml(err.message || err))
-}
+exports.warning = function warning(err) {
+  console.error(err.stack || err.message || err);
+  return exports.log(escapeHtml(err.message || err));
+};
 
-                exports.error = function error(err) {
-  console.error(err.stack || err.message || err)
-  var p = exports.log(escapeHtml(err.message || err))
-  p.style.color = 'red'
-  p.style.fontWeight = 'bold'
-  return p
-}
+exports.error = function error(err) {
+  console.error(err.stack || err.message || err);
+  var p = exports.log(escapeHtml(err.message || err));
+  p.style.color = "red";
+  p.style.fontWeight = "bold";
+  return p;
+};
